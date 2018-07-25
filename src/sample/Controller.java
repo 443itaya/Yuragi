7bac08d9d1b27d52de7807f6e86ff1fcd99c6072
443itaya <yoshimi.itaya@gmail.com>
2018-07-26 07:39:19 +0900

    sin

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -8,18 +8,23 @@ import java.io.IOException;
 
 
 public class Controller {
-    String url = "http://172.20.11.99/api/koAdybYd5hmMNfAZ8R4orVmb6bqrTY2S9nG4XZww/groups/1/action";
+    String url = "http://172.20.11.100/api/z2YrJsBIMyPZlHWprsFmIjlfI2WaR9kxTHA6XVaI/groups/1/action";
     String json;
     private String res = "";
 
     @FXML
     public void onStartButtonClick(ActionEvent event) {
         System.out.println("start");
-        for(int i = 0; i < 255; i+=30) {
-            System.out.println(i);
-            String bri = String.valueOf(i);
-            json = "{\"on\":true,\"hue\":14988,\"bri\":"+bri+",\"sat\":141}";
-            postTest();
+        try {
+            for (int i = 0; i <= 360; i += 10) {
+                System.out.println(i);
+                String bri = String.valueOf((int)(127*Math.sin(Math.toRadians(i))+127));
+                json = "{\"on\":true,\"hue\":14988,\"bri\":" + bri + ",\"sat\":141}";
+                postTest();
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
     }
@@ -44,13 +49,13 @@ public class Controller {
         client.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
-                //System.out.println("failure");
+                System.out.println("failure");
             }
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
-                //res = response.body().string();
-                //System.out.println(res);
+                res = response.body().string();
+                System.out.println(res);
             }
         });
     }
